5d0e2b11e90163826c0186c56a961e63fa2ef445
NikG18 <ntganzha1804@outlook.com>
2026-08-12 10:46:44 +0300

    Update payments.py

--- payments.py
+++ payments.py
@@ -9,7 +9,7 @@ TINKOFF_TERMINAL_KEY = os.environ.get("TINKOFF_TERMINAL_KEY")
 TINKOFF_SECRET_KEY = os.environ.get("TINKOFF_SECRET_KEY")
 
 # Для боевого API
-API_BASE = "https://rest-api-test.tinkoff.ru/v2/"
+API_BASE = "https://securepay.tinkoff.ru/v2/"
 
 def generate_token(params: dict) -> str:
     excluded_keys = {"Token", "Receipt", "DATA", "Shops", "Receipts", "PaymentMethods"}
