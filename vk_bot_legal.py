a2ddde1db842f63d3dcd44cc21897097f9374cf2
NikG18 <ntganzha1804@outlook.com>
2026-08-29 23:54:34 +0200

    Load VK legal message dedupe last

--- vk_bot_legal.py
+++ vk_bot_legal.py
@@ -1,6 +1,7 @@
 import vk_bot as app
 import payment_reuse_vk  # noqa: F401  # один T-Bank платёж на бронь, повторная выдача СБП-ссылки
 import legal_vk  # noqa: F401  # юридический слой оборачивает итоговый платёжный сценарий
+import legal_vk_dedupe  # noqa: F401  # последний слой убирает повторную отправку документов
 
 
 async def main():
