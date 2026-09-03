4061ef256d72ba25b99a56bdbb28d6e2a46d7512
NikG18 <ntganzha1804@outlook.com>
2026-09-03 23:09:20 +0200

    Use booking platform for cross messenger tutor confirmations

--- vk_bot_legal.py
+++ vk_bot_legal.py
@@ -4,7 +4,7 @@ import legal_vk  # noqa: F401
 import legal_vk_dedupe  # noqa: F401
 from financial_hardening import install_financial_hardening
 from student_stats_hardening import install_student_stats_hardening
-from student_account_hardening import install_student_account_hardening
+from student_account_hardening import install_student_account_hardening, install_booking_account_context
 from pricing_text_hardening import install_pricing_text_hardening
 from tutor_archive_hardening import install_tutor_archive_hardening
 from subscription_booking import install_vk_subscription_booking
@@ -24,6 +24,7 @@ install_student_account_hardening(app, "vk")
 install_pricing_text_hardening(app)
 install_tutor_archive_hardening(app)
 install_vk_subscription_booking(app)
+install_booking_account_context(app, "vk")
 install_subscription_cancel_release(app)
 install_vk_completion_hardening(app)
 install_vk_payment_hardening(app)
