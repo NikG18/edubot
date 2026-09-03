efd9842b18fe4fa854f87c4ad17dcea7a3a1071a
NikG18 <ntganzha1804@outlook.com>
2026-09-03 22:26:18 +0200

    Remove learning materials from VK entirely

--- vk_bot_legal.py
+++ vk_bot_legal.py
@@ -8,16 +8,18 @@ from subscription_booking import install_vk_subscription_booking
 from subscription_cancel_hardening import install_subscription_cancel_release
 from completion_hardening import install_vk_completion_hardening
 from vk_payment_hardening import install_vk_payment_hardening
+from access_hardening import install_vk_materials_guard
 from runtime_hardening import install_vk_hardening
 
 # В VK остаются пользовательские/преподавательские сценарии и статистика. Факт проведения
-# подтверждается только в Telegram-админке, а identity/оплата общие с Telegram.
+# и учебные материалы доступны администратору только в Telegram.
 install_financial_hardening(app)
 install_student_stats_hardening(app)
 install_vk_subscription_booking(app)
 install_subscription_cancel_release(app)
 install_vk_completion_hardening(app)
 install_vk_payment_hardening(app)
+install_vk_materials_guard(app)
 install_vk_hardening(app)
 
 
