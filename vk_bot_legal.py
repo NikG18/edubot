08dee75da8e78c126c068d41fa4b7fbedb0a038c
NikG18 <ntganzha1804@outlook.com>
2026-09-03 22:29:58 +0200

    Use centralized current pricing text in VK

--- vk_bot_legal.py
+++ vk_bot_legal.py
@@ -1,9 +1,10 @@
 import vk_bot as app
-import payment_reuse_vk  # noqa: F401  # один T-Bank платёж на бронь, повторная выдача СБП-ссылки
-import legal_vk  # noqa: F401  # юридический слой оборачивает итоговый платёжный сценарий
-import legal_vk_dedupe  # noqa: F401  # последний слой убирает повторную отправку документов
+import payment_reuse_vk  # noqa: F401
+import legal_vk  # noqa: F401
+import legal_vk_dedupe  # noqa: F401
 from financial_hardening import install_financial_hardening
 from student_stats_hardening import install_student_stats_hardening
+from pricing_text_hardening import install_pricing_text_hardening
 from tutor_archive_hardening import install_tutor_archive_hardening
 from subscription_booking import install_vk_subscription_booking
 from subscription_cancel_hardening import install_subscription_cancel_release
@@ -12,10 +13,9 @@ from vk_payment_hardening import install_vk_payment_hardening
 from access_hardening import install_vk_materials_guard
 from runtime_hardening import install_vk_hardening
 
-# В VK остаются пользовательские/преподавательские сценарии и статистика. Факт проведения
-# и учебные материалы доступны администратору только в Telegram.
 install_financial_hardening(app)
 install_student_stats_hardening(app)
+install_pricing_text_hardening(app)
 install_tutor_archive_hardening(app)
 install_vk_subscription_booking(app)
 install_subscription_cancel_release(app)
