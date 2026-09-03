9aaba3638eacda2b57de77adb83bda045abc0af0
NikG18 <ntganzha1804@outlook.com>
2026-09-03 23:13:40 +0200

    Install unified cross platform tutor confirmation in VK

--- vk_bot_legal.py
+++ vk_bot_legal.py
@@ -4,10 +4,11 @@ import legal_vk  # noqa: F401
 import legal_vk_dedupe  # noqa: F401
 from financial_hardening import install_financial_hardening
 from student_stats_hardening import install_student_stats_hardening
-from student_account_hardening import install_student_account_hardening, install_booking_account_context
+from student_account_hardening import install_student_account_hardening
 from pricing_text_hardening import install_pricing_text_hardening
 from tutor_archive_hardening import install_tutor_archive_hardening
 from subscription_booking import install_vk_subscription_booking
+from tutor_confirmation_hardening import install_vk_tutor_confirmation
 from subscription_cancel_hardening import install_subscription_cancel_release
 from completion_hardening import install_vk_completion_hardening
 from vk_payment_hardening import install_vk_payment_hardening
@@ -25,7 +26,7 @@ install_student_account_hardening(app, "vk")
 install_pricing_text_hardening(app)
 install_tutor_archive_hardening(app)
 install_vk_subscription_booking(app)
-install_booking_account_context(app, "vk")
+install_vk_tutor_confirmation(app)
 install_subscription_cancel_release(app)
 install_vk_completion_hardening(app)
 install_vk_payment_hardening(app)
