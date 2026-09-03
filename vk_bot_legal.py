efbf37e9b46efa796fa507187f66632be22cfc92
NikG18 <ntganzha1804@outlook.com>
2026-09-03 22:23:55 +0200

    Use booking-linked T-Bank payment menu in VK

--- vk_bot_legal.py
+++ vk_bot_legal.py
@@ -6,14 +6,16 @@ from financial_hardening import install_financial_hardening
 from subscription_booking import install_vk_subscription_booking
 from subscription_cancel_hardening import install_subscription_cancel_release
 from completion_hardening import install_vk_completion_hardening
+from vk_payment_hardening import install_vk_payment_hardening
 from runtime_hardening import install_vk_hardening
 
 # В VK остаются пользовательские/преподавательские сценарии и статистика. Факт проведения
-# подтверждается только в Telegram-админке, но одинаковые правила БД применяются в обоих процессах.
+# подтверждается только в Telegram-админке, а оплата использует тот же booking-linked T-Bank flow.
 install_financial_hardening(app)
 install_vk_subscription_booking(app)
 install_subscription_cancel_release(app)
 install_vk_completion_hardening(app)
+install_vk_payment_hardening(app)
 install_vk_hardening(app)
 
 
