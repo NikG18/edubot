462824a38bfd3337f0cecab08192375e2ce99cd4
NikG18 <ntganzha1804@outlook.com>
2026-09-04 18:00:53 +0200

    Install core booking flow fixes in Telegram

--- Bot_test_legal.py
+++ Bot_test_legal.py
@@ -25,6 +25,7 @@ from delivery_hardening import install_delivery_hardening
 from contact_delivery_hardening import install_telegram_contact_delivery_hardening
 from telegram_messaging_identity_hardening import install_telegram_messaging_identity_hardening
 from pagination_hardening import install_telegram_pagination_hardening
+from core_flow_hardening import install_telegram_core_flow_hardening
 from runtime_hardening import install_telegram_hardening
 
 install_financial_hardening(app)
@@ -51,6 +52,7 @@ install_delivery_hardening(app)
 install_telegram_contact_delivery_hardening(app)
 install_telegram_messaging_identity_hardening(app)
 install_telegram_pagination_hardening(app)
+install_telegram_core_flow_hardening(app)
 install_telegram_hardening(app)
 
 
